de0b1b935bda1bd2bb29b44a54de84fb1ef4533e
laichenlin <534140200@qq.com>
2019-06-05 23:28:24 +0800

    初始化项目

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'hello world11';
+    }
+
+    public function test()
+    {
+        echo 124563;
+
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'hello world';
+    }
+
+    public function test()
+    {
+        echo 124563;
+
+    }
+}
