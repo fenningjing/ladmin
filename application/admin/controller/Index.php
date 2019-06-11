a983e2f498d11ecf0e9d0606158f126c43835035
laichenlin <534140200@qq.com>
2019-06-11 08:35:20 +0800

    测试

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,10 +1,19 @@
 <?php
+
 namespace app\admin\controller;
+use app\admin\model\AdminAccount;
 
 class Index
 {
     public function index()
     {
+        $adminAccont = new AdminAccount;
+        $adminAccont->account = 'test';
+        $adminAccont->phone = '15059945236';
+        $result = $adminAccont->save();
+
+        var_dump($result);
+        exit;
         return 'hello world11';
     }
 
